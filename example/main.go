290970451bbac5e204e7316ea0869f6e19ff0a1e
michael zhu <tianying484@gmail.com>
2020-12-10 22:19:00 +0800

    rewrite looker sdk for golang

---
+++ example/main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+    "encoding/json"
+    "github.com/tianying484/looker/rtl"
+    v4 "github.com/tianying484/looker/sdk/v4"
+    "log"
+)
+
+func main() {
+    var (
+        err          error
+        lookerId     int64  = 465
+        limit        int64  = 5000
+        resultFormat string = "csv"
+    )
+    // Read settings from ini file
+    cfg, err := rtl.NewSettingsFromFile("./looker.ini", nil)
+    checkErr(err)
+
+    // New instance of LookerSDK
+    sdk := v4.NewLookerSDK(rtl.NewAuthSession(cfg))
+
+    // get looker info
+    looker, err := sdk.Look(lookerId, nil, nil)
+    checkErr(err)
+
+    // get looker filters
+    filters, err := json.MarshalIndent(looker.Query.Filters, "  ", "  ")
+    checkErr(err)
+    log.Println("get looker filters", *looker.QueryId, string(filters))
+
+    // create looker query request
+    createQuery, err := sdk.CreateQuery(v4.WriteQuery{
+        Model:         looker.Query.Model,
+        View:          looker.Query.View,
+        Fields:        looker.Query.Fields,
+        Pivots:        looker.Query.Pivots,
+        FillFields:    looker.Query.FillFields,
+        Filters:       looker.Query.Filters,
+        Sorts:         looker.Query.Sorts,
+        Limit:         looker.Query.Limit,
+        ColumnLimit:   looker.Query.ColumnLimit,
+        Total:         looker.Query.Total,
+        RowTotal:      looker.Query.RowTotal,
+        Subtotals:     looker.Query.Subtotals,
+        DynamicFields: looker.Query.DynamicFields,
+        QueryTimezone: looker.Query.QueryTimezone,
+    }, nil, nil)
+    checkErr(err)
+    log.Println("create query success, query_id=", *createQuery.Id)
+
+    // run looker query
+    csvText, err := sdk.RunQuery(v4.RequestRunQuery{
+        QueryId:      *createQuery.Id,
+        ResultFormat: resultFormat,
+        Limit:        &limit,
+    }, nil)
+    checkErr(err)
+
+    log.Println("run query result", csvText)
+
+    // List all users in Looker
+    users, err := sdk.AllUsers(v4.RequestAllUsers{}, nil)
+    checkErr(err)
+
+    log.Println("-------------------------")
+    // Iterate the users and print basic user info
+    for _, u := range users {
+        log.Printf("user: %s:%s:%s\n", *u.FirstName, *u.LastName, *u.Email)
+    }
+    log.Println("-------------------------")
+}
+
+func checkErr(err error) {
+    if err != nil {
+        log.Fatal(err)
+    }
+}
---
+++ rtl/settings.go
@@ -0,0 +1,64 @@
+package rtl
+
+import (
+    "fmt"
+    "gopkg.in/ini.v1"
+)
+
+var defaultSectionName string = "Looker"
+
+type ApiSettings struct {
+    BaseUrl      string `ini:"base_url"`
+    VerifySsl    bool   `ini:"verify_ssl"`
+    Timeout      int32  `ini:"timeout"`
+    AgentTag     string `ini:"agent_tag"`
+    FileName     string `ini:"file_name"`
+    ClientId     string `ini:"client_id"`
+    ClientSecret string `ini:"client_secret"`
+    ApiVersion   string `ini:"api_version"`
+}
+
+func NewSettings() *ApiSettings {
+    return &ApiSettings{}
+}
+
+func (a *ApiSettings) WithBaseUrl(BaseUrl string) *ApiSettings {
+    a.BaseUrl = BaseUrl
+    return a
+}
+func (a *ApiSettings) WithVerifySsl(VerifySsl bool) *ApiSettings {
+    a.VerifySsl = VerifySsl
+    return a
+}
+func (a *ApiSettings) WithTimeout(Timeout int32) *ApiSettings {
+    a.Timeout = Timeout
+    return a
+}
+func (a *ApiSettings) WithClientId(ClientId string) *ApiSettings {
+    a.ClientId = ClientId
+    return a
+}
+func (a *ApiSettings) WithClientSecret(ClientSecret string) *ApiSettings {
+    a.ClientSecret = ClientSecret
+    return a
+}
+
+func NewSettingsFromFile(file string, section *string) (ApiSettings, error) {
+    if section == nil {
+        section = &defaultSectionName
+    }
+
+    s := ApiSettings{
+        VerifySsl:  true,
+        ApiVersion: DefaultApiVersion,
+    }
+
+    cfg, err := ini.Load(file)
+    if err != nil {
+        return s, fmt.Errorf("error reading ini file: %w", err)
+    }
+
+    err = cfg.Section(*section).MapTo(&s)
+    return s, err
+
+}
